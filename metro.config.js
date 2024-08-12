459865ba98a5f545b8b788ff8919f0a9c9f8b790
monarcode <ugbahisioma@gmail.com>
2024-08-12 11:54:48 +0100

    ✨ ft(app): setup svg-transformer for improved svg support

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,20 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  // eslint-disable-next-line no-undef
+  const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return config;
+})();
