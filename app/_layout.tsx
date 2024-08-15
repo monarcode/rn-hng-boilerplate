bf2d237aa8e3dec6ce9d33ea0e5c5651a06b5935
monarcode <ugbahisioma@gmail.com>
2024-08-15 13:29:32 +0100

    ✨ ft(tools): setup tanstack-query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,12 +9,14 @@ import {
   useFonts,
 } from '@expo-google-fonts/inter';
 import { PortalHost } from '@rn-primitives/portal';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { router, SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import Toast from 'react-native-toast-message';
 
+import { queryClient } from '~/libs/query';
 import { toastConfig } from '~/libs/toast-config';
 import useAuthStore from '~/store/auth';
 
@@ -66,15 +68,17 @@ function Root() {
 
   return (
     <>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-          <Stack.Screen name="(create-product)" options={{ headerShown: false }} />
-          <Stack.Screen name="user" options={{ headerShown: false }} />
-        </Stack>
-      </GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+            <Stack.Screen name="(create-product)" options={{ headerShown: false }} />
+            <Stack.Screen name="user" options={{ headerShown: false }} />
+          </Stack>
+        </GestureHandlerRootView>
+      </QueryClientProvider>
       <Toast config={toastConfig} />
       <PortalHost />
     </>
