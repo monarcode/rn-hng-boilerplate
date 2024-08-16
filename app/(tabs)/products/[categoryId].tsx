3e6c400ad365f0d7d7e7c2773ffe67e067636f2d
Zeemari <zenntia@gmail.com>
2024-08-16 02:13:40 +0100

    ft: product category feature

--- app/(tabs)/products/[categoryId].tsx
+++ app/(tabs)/products/[categoryId].tsx
@@ -1,10 +1,15 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable, Image, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '~/components/shared';
 import { THEME } from '~/constants/theme';
 import { useGlobalSearchParams, useLocalSearchParams } from 'expo-router';
 import { useProducts } from '~/hooks/products/organization/fetchProducts';
 import useAuthStore from '~/store/auth';
+import ProductItemListTile from '~/components/product-list/product-item-list';
+import { SearchAndFilter} from '~/components/product-list';
+import { Plus } from 'react-native-feather';
+import { router } from 'expo-router';
+import GoBack from '~/components/go-back';
 
 const ViewProductsByCategory = () => {
   const authstore = useAuthStore();
@@ -12,22 +17,64 @@ const ViewProductsByCategory = () => {
   const { categoryId } = useLocalSearchParams();
   const { data } = useProducts(orgId);
   const category = data?.find((item) => item.name == categoryId);
+  // console.log(category);
 
   return (
     <View style={styles.container}>
-      <Text>ViewProductsByCategory</Text>
+      <View style={styles.headerContainer}>
+        <GoBack />
+        <Text size="3xl" weight="bold">{category?.name}</Text>
+        <TouchableOpacity
+          onPress={() => { /* menu logic */}}>
+          <Image source={require('~/assets/menu.png')} />
+        </TouchableOpacity>
+      </View>
+      <SearchAndFilter />
+      <View style={styles.itemListStyle}>
+      <ProductItemListTile {...category?.products[0]} />
+      </View>
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => {
+          router.push('/(create-product)/create-product');
+        }}>
+        <Plus width={25} height={25} color={THEME.colors.white} />
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: THEME.colors.white,
     flex: 1,
+    gap: THEME.spacing.lg,
+  },
+  floatingButton: {
+    position: 'absolute',
+    borderRadius: 1000,
+    height: 50,
+    width: 50,
+    backgroundColor: THEME.colors.primary,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: THEME.spacing.gutter,
-    backgroundColor: THEME.colors.white,
+    right: THEME.spacing.md,
+    bottom: 10,
   },
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: THEME.colors.border,
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  itemListStyle: {
+    padding: THEME.spacing.md
+  }
 });
 
 export default ViewProductsByCategory;
