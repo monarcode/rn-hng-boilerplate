c46aa0abcb3fa3fc03591e3b31a288775f6183f0
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-18 20:41:29 +0100

    refactor: Update dashboard hook to use single quotes and improve formatting

--- hooks/dashboard/dashboard.ts
+++ hooks/dashboard/dashboard.ts
@@ -1,16 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchDashboard } from "~/services/dashboard";
+import { useQuery } from '@tanstack/react-query';
 
+import { fetchDashboard } from '~/services/dashboard';
 
- const useDashboard=(userId:string|undefined)=>{
-    const {data,isLoading,isError}=useQuery(
-        {
-            queryKey:['dashboard'],
-            queryFn:()=>fetchDashboard(userId)
-        }
-    )
+const useDashboard = (userId: string | undefined) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['dashboard'],
+    queryFn: () => fetchDashboard(userId),
+  });
 
-    return {data,isError,isLoading}
-}
+  return { data, isError, isLoading };
+};
 
-export {useDashboard}
+export { useDashboard };
