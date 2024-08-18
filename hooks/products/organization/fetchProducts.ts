cf5711393a9fdced3cf08e774a1779483d392053
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-18 20:41:37 +0100

    refactor: Update fetchProducts hook to use single quotes and improve formatting

--- hooks/products/organization/fetchProducts.ts
+++ hooks/products/organization/fetchProducts.ts
@@ -1,14 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-import { http } from "~/libs/ky";
-import { ProductService } from "~/services/product";
+import { useQuery } from '@tanstack/react-query';
 
+import { http } from '~/libs/ky';
+import { ProductService } from '~/services/product';
 
-export const useProducts=(orgId:string|undefined)=>{
-    const {data,isError,isLoading}=useQuery(
-        {
-            queryKey:['product'],
-            queryFn:()=>ProductService.fetchProducts(orgId)
-        }
-    )
-    return {data,isError,isLoading}
-}
+export const useProducts = (orgId: string | undefined) => {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['product'],
+    queryFn: () => ProductService.fetchProducts(orgId),
+  });
+  return { data, isError, isLoading };
+};
