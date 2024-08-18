9bed4463650b38e5344a240cbacc60e85eaeda4d
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-18 20:42:52 +0100

    refactor: Update fetchDashboard function to use single quotes and improve formatting

--- services/dashboard.ts
+++ services/dashboard.ts
@@ -1,20 +1,19 @@
-import { http } from "~/libs/ky";
-import { DashboardResponse,DashboardResponseError } from "~/types/dashboard/dashboard";
-import { HTTPError } from "ky";
+import { HTTPError } from 'ky';
 
+import { http } from '~/libs/ky';
+import { DashboardResponse, DashboardResponseError } from '~/types/dashboard/dashboard';
 
-const fetchDashboard=async (userId:string|undefined):Promise<DashboardResponse>=>{
-    try{
-        const response=await http.get(`Dashboards?userId=${userId}`).json<DashboardResponse>()
-        return response
-    }
-    catch(error){
+const fetchDashboard = async (userId: string | undefined): Promise<DashboardResponse> => {
+  try {
+    const response = await http.get(`Dashboards?userId=${userId}`).json<DashboardResponse>();
+    return response;
+  } catch (error) {
     if (error instanceof HTTPError) {
       const errorBody = await error.response.json<DashboardResponseError>();
       throw new Error(errorBody.message || `HTTP error ${error.response.status}`);
     }
     throw error;
   }
-}
+};
 
-export {fetchDashboard}
+export { fetchDashboard };
