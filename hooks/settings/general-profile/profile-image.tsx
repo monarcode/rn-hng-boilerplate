822cf0b8798ab70260106981b381c32913f00bed
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-19 22:48:45 +0100

    refactor: Add useProfileImage hook for profile image functionality

---
+++ hooks/settings/general-profile/profile-image.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import Toast from 'react-native-toast-message';
+
+import { useDeleteProfilePicture } from '~/hooks/settings/deleteProfilePicture';
+import { useUpdateProfilePicture } from '~/hooks/settings/updateProfilePicture';
+import { queryClient } from '~/libs/query';
+import { pickImageOrUseCamera, resizeImage } from '~/utils/profile-image-handler';
+
+type ImageState = {
+  uri: string | undefined;
+  status: 'idle' | 'loading' | 'success' | 'error';
+  error?: string;
+};
+
+export const useProfileImage = (userEmail: string) => {
+  const [selectedImage, setSelectedImage] = useState<ImageState>({
+    uri: '',
+    status: 'idle',
+  });
+
+  const { mutate: deleteProfilePicture } = useDeleteProfilePicture();
+  const { mutate: updateProfilePicture } = useUpdateProfilePicture();
+
+  const handleRemovePhoto = async () => {
+    setSelectedImage({ uri: undefined, status: 'loading' });
+    try {
+      await deleteProfilePicture(userEmail);
+      Toast.show({
+        type: 'success',
+        props: { title: 'Success', description: 'Profile picture removed successfully' },
+      });
+    } catch (error) {
+      setSelectedImage((prev) => ({
+        ...prev,
+        status: 'error',
+        error: 'Failed to remove image',
+      }));
+      Toast.show({
+        type: 'error',
+        props: { title: 'Error', description: 'Failed to remove image' },
+      });
+    } finally {
+      setSelectedImage({ uri: undefined, status: 'idle' });
+      queryClient.invalidateQueries({ queryKey: ['profile'] });
+    }
+  };
+
+  const handleImagePick = async (): Promise<void> => {
+    try {
+      const imageUri = await pickImageOrUseCamera();
+
+      if (imageUri) {
+        const resizedImageUri = await resizeImage(imageUri);
+        updateProfilePicture(
+          { email: userEmail, photo: resizedImageUri },
+          {
+            onSuccess: (response) => {
+              if (response && response.status_code === 200) {
+                setSelectedImage({ uri: response.data.avatar_url, status: 'success' });
+                Toast.show({
+                  type: 'success',
+                  props: { title: 'Success', description: 'Profile picture updated successfully' },
+                });
+              } else {
+                throw new Error('Failed to update profile picture');
+              }
+            },
+            onError: (error) => {
+              console.error('Error updating profile picture:', error);
+              Toast.show({
+                type: 'error',
+                props: { title: 'Error', description: 'Failed to update profile picture' },
+              });
+            },
+          }
+        );
+      }
+    } catch (error) {
+      setSelectedImage((prev) => ({ ...prev, status: 'error', error: 'Failed to pick image' }));
+      Toast.show({
+        type: 'error',
+        props: { title: 'Error', description: 'Failed to pick image' },
+      });
+    }
+  };
+
+  return { selectedImage, handleRemovePhoto, handleImagePick };
+};
