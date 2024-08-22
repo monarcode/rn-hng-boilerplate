b4818b66f9b073086dbd010973911ddef43f49d9
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-22 01:34:04 +0100

    refactor: Remove optional fields from edit profile form schema

--- modules/settings/validation-schema/edit-profile.ts
+++ modules/settings/validation-schema/edit-profile.ts
@@ -2,11 +2,5 @@ import { z } from 'zod';
 
 export const editProfileFormSchema = z.object({
   user_name: z.string().min(3, 'Username must be at least 3 characters'),
-  pronoun: z.string().optional(),
-  job_title: z.string().optional(),
-  department: z.string().optional(),
   bio: z.string().max(64, 'Bio must not exceed 64 characters').optional(),
-  twitter_link: z.string().url('Invalid URL').optional().or(z.literal('')),
-  facebook_link: z.string().url('Invalid URL').optional().or(z.literal('')),
-  linkedin_link: z.string().url('Invalid URL').optional().or(z.literal('')),
 });
