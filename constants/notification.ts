547a66d13b5186c6e30a5bd1e49c1612479ae175
oloyedewaris <oloyedewaris@gmail.com>
2024-08-22 10:26:18 +0100

    imp: removed unused sections and added saved dialog

--- constants/notification.ts
+++ constants/notification.ts
@@ -34,31 +34,6 @@ const notificationSections = [
       },
     ],
   },
-  {
-    title: 'Slack Notification',
-    data: [
-      {
-        header: 'Activity in your workspace',
-        body: 'Receive emails when you get comments, mentions, page invites, reminders, access requests, and property changes',
-        status: false,
-      },
-      {
-        header: 'Always send email notifications',
-        body: 'Receive emails about activity in your workspace, even when you are active on the app',
-        status: true,
-      },
-      {
-        header: 'Email digests',
-        body: 'Receive email digest every 8 hours for changes to pages you are subscribed to',
-        status: false,
-      },
-      {
-        header: 'Announcement and update emails',
-        body: 'Receive occasional emails about product launches and new features from notion',
-        status: false,
-      },
-    ],
-  },
 ];
 
 export default notificationSections;
