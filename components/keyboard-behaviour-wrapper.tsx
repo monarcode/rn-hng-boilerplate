b3a0d124fcb2cb85b6fbb5832d56e8d83162c657
monarcode <ugbahisioma@gmail.com>
2024-08-23 16:45:06 +0100

    🐛 bug(tools): update keyboard wrapper styles
    various scroll issues were detected from this component, this commit fixes that

--- components/keyboard-behaviour-wrapper.tsx
+++ components/keyboard-behaviour-wrapper.tsx
@@ -1,12 +1,17 @@
 import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from 'react-native';
 
+import { View } from './shared';
+
 const KeyboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={{ flex: 1 }}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>{children}</TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1 }}>{children}</View>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 };
+
 export default KeyboardWrapper;
