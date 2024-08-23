4555f8ec92bbe2c8b752f69a51ed2895f60b5179
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-23 19:28:53 +0100

    refactor: Update updateUserPassword function to include old_password, new_password, and confirm_new_password parameters

--- services/password-update.ts
+++ services/password-update.ts
@@ -2,11 +2,15 @@ import { HTTPError } from 'ky';
 
 import { http } from '~/libs/ky';
 
-const updateUserPassword = async (payload: any): Promise<any> => {
+const updateUserPassword = async (payload: {
+  old_password: string;
+  new_password: string;
+  confirm_new_password: string;
+}): Promise<any> => {
   try {
     const response = await http
       .put('auth/password', {
-        json: { ...payload },
+        json: payload,
       })
       .json();
     console.log({ response });
@@ -14,7 +18,12 @@ const updateUserPassword = async (payload: any): Promise<any> => {
   } catch (error) {
     if (error instanceof HTTPError) {
       const errorBody = await error.response.json<any>();
-      throw new Error(errorBody.message || `HTTP error ${error.response.status}`);
+      console.log('errorBody', errorBody);
+      throw new Error(
+        Array.isArray(errorBody.message)
+          ? errorBody.message.map((m: any) => m.message).join(', ')
+          : errorBody.message || `HTTP error ${error.response.status}`
+      );
     }
     throw error;
   }
