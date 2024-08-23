328b11d9982e90cbf3b44510edf23c294e45cf8a
Zeemari <zenntia@gmail.com>
2024-08-23 15:20:55 +0100

    ft: order screen implementation

---
+++ app/(tabs)/orders/[detailsId].tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const OrderDetails = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Orders Detailed page!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,          
+        justifyContent: 'center', 
+        alignItems: 'center',     
+    },
+ })
+
+export default OrderDetails;
