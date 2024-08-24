fb2cbb89a2aeac4354400b581e6cd87513ae9065
oloyedewaris <oloyedewaris@gmail.com>
2024-08-24 14:52:08 +0100

    imp: implemented for more languages

--- app/(tabs)/orders/index.tsx
+++ app/(tabs)/orders/index.tsx
@@ -1,97 +1,101 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Image, FlatList, ListRenderItem } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { ScrollView } from 'react-native-gesture-handler';
+
 import { Text, View } from '~/components/shared';
 import { THEME } from '~/constants/theme';
 
 import GoBack from '~/components/go-back';
-import { ScrollView } from 'react-native-gesture-handler';
 
 type Order = {
-    id: string;
-    orderNumber: string;
-    date: string;
-    estimatedDate: string;
-    isDelivered: boolean;
-    productImage: any;
-  };
-  
-  const OrdersList: React.FC = () => {
-    const orders: Order[] = [
-      {
-        id: '1',
-        orderNumber: '99012',
-        date: '20-Aug-2024 7:41PM',
-        estimatedDate: '26th Aug',
-        isDelivered: false,
-        productImage: require('~/assets/product.png'),
-      },
-      {
-        id: '2',
-        orderNumber: '99013',
-        date: '21-Aug-2024 3:22PM',
-        estimatedDate: '27th Aug',
-        isDelivered: false,
-        productImage: require('~/assets/product.png'),
-      },
-      {
-        id: '3',
-        orderNumber: '99014',
-        date: '22-Aug-2024 1:12PM',
-        estimatedDate: '30th Aug',
-        isDelivered: true,
-        productImage: require('~/assets/product.png'),
-      },
-    ];
-  
-    const renderOrderItem: ListRenderItem<Order> = ({ item }) => (
-      <View style={styles.orderCard}>
-        <View style={styles.orderInfo}>
-          <Text weight='bold' style={styles.orderNumber}>Order#: {item.orderNumber}</Text>
-          <Text style={styles.orderDate}> {item.date} </Text>
-          <View style={styles.deliveryInfo}>
-            <Image source={require('~/assets/delivery-icon.png')} style={styles.deliveryIcon} />
-            <Text
-              weight='medium'
-              style={[
-                styles.deliveryText,
-                { color: item.isDelivered ? THEME.colors.red : THEME.colors.green }
-              ]}
-            >
-              {item.isDelivered ? `Delivered on ${item.estimatedDate}` : `Estimated Delivery on ${item.estimatedDate}`}
-            </Text>
-          </View>
-        </View>
-        <View style={styles.productImageStyle}>
-          <Image source={item.productImage} style={styles.productImage} />
+  id: string;
+  orderNumber: string;
+  date: string;
+  estimatedDate: string;
+  isDelivered: boolean;
+  productImage: any;
+};
+
+const OrdersList: React.FC = () => {
+  const { t } = useTranslation();
+
+  const orders: Order[] = [
+    {
+      id: '1',
+      orderNumber: '99012',
+      date: '20-Aug-2024 7:41PM',
+      estimatedDate: '26th Aug',
+      isDelivered: false,
+      productImage: require('~/assets/product.png'),
+    },
+    {
+      id: '2',
+      orderNumber: '99013',
+      date: '21-Aug-2024 3:22PM',
+      estimatedDate: '27th Aug',
+      isDelivered: false,
+      productImage: require('~/assets/product.png'),
+    },
+    {
+      id: '3',
+      orderNumber: '99014',
+      date: '22-Aug-2024 1:12PM',
+      estimatedDate: '30th Aug',
+      isDelivered: true,
+      productImage: require('~/assets/product.png'),
+    },
+  ];
+
+  const renderOrderItem: ListRenderItem<Order> = ({ item }) => (
+    <View style={styles.orderCard}>
+      <View style={styles.orderInfo}>
+        <Text weight='bold' style={styles.orderNumber}>Order#: {item.orderNumber}</Text>
+        <Text style={styles.orderDate}> {item.date} </Text>
+        <View style={styles.deliveryInfo}>
+          <Image source={require('~/assets/delivery-icon.png')} style={styles.deliveryIcon} />
+          <Text
+            weight='medium'
+            style={[
+              styles.deliveryText,
+              { color: item.isDelivered ? THEME.colors.red : THEME.colors.green }
+            ]}
+          >
+            {item.isDelivered ? `Delivered on ${item.estimatedDate}` : `Estimated Delivery on ${item.estimatedDate}`}
+          </Text>
         </View>
       </View>
-    );
-  
-    return (
-      <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <View style={styles.orderStyle}>
-            <GoBack />
-            <Text size="2xl" weight="bold"> Orders </Text>
-          </View>
-          <TouchableOpacity onPress={() => { /* menu logic */ }}>
-            <Image source={require('~/assets/search.png')} />
-          </TouchableOpacity>
-        </View>
-  
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.orderItem}>
-            <FlatList
-              data={orders}
-              renderItem={renderOrderItem}
-              keyExtractor={(item) => item.id}
-              contentContainerStyle={styles.listContentContainer}
-            />
-          </View>
-        </ScrollView>
+      <View style={styles.productImageStyle}>
+        <Image source={item.productImage} style={styles.productImage} />
       </View>
-    );
-  };
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <View style={styles.orderStyle}>
+          <GoBack />
+          <Text size="2xl" weight="bold"> {t('Orders')} </Text>
+        </View>
+        <TouchableOpacity onPress={() => { /* menu logic */ }}>
+          <Image source={require('~/assets/search.png')} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.orderItem}>
+          <FlatList
+            data={orders}
+            renderItem={renderOrderItem}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={styles.listContentContainer}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
