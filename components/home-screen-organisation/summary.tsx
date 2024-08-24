69f7a4dea7043fd32f5e96fb0b7ce5684dc1223b
monarcode <ugbahisioma@gmail.com>
2024-08-24 17:35:52 +0100

    misc

--- components/home-screen-organisation/summary.tsx
+++ components/home-screen-organisation/summary.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { useTranslation } from 'react-i18next';
 
 import ActiveMembers from '../../assets/icons/activemembers.svg';
 import AllMembers from '../../assets/icons/allmembers.svg';
