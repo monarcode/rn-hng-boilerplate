7fa9ed37851c321d0d4851c8193b810575a1bb3c
MakindeTobby <makinde.tobiloba@gmail.com>
2024-08-24 18:50:03 +0100

    chore: switched to keyboard aware

--- app/(create-product)/create-product.tsx
+++ app/(create-product)/create-product.tsx
@@ -1,5 +1,7 @@
+import { Stack } from 'expo-router';
 import { ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import BasicHeader from '~/components/basic-header';
 
 import GoBack from '~/components/go-back';
 import KeyboardAwareWrapper from '~/components/keyboard-aware-wrapper';
@@ -9,25 +11,21 @@ import CreateProductForm from '~/modules/products/components/create-product-form
 
 const CreateProductScreen = () => {
   return (
-    <KeyboardAwareWrapper>
-      <SafeAreaView
-        edges={['top', 'bottom']}
-        style={[styles.container, { paddingHorizontal: THEME.spacing.gutter }]}>
-        <View style={styles.header}>
-          <GoBack />
-          <Text weight="semiBold" size="xl">
-            Add a Product
-          </Text>
-          <View style={styles.iconPlaceholder} />
-        </View>
-        <ScrollView
-          style={styles.container}
-          contentContainerStyle={styles.contentContainer}
-          showsVerticalScrollIndicator={false}>
-          <CreateProductForm />
-        </ScrollView>
-      </SafeAreaView>
-    </KeyboardAwareWrapper>
+    <>
+      <BasicHeader label="Add a Product" />
+      <KeyboardAwareWrapper>
+        <SafeAreaView
+          edges={['top', 'bottom']}
+          style={[styles.container, { paddingHorizontal: THEME.spacing.gutter }]}>
+          <ScrollView
+            style={styles.container}
+            contentContainerStyle={styles.contentContainer}
+            showsVerticalScrollIndicator={false}>
+            <CreateProductForm />
+          </ScrollView>
+        </SafeAreaView>
+      </KeyboardAwareWrapper>
+    </>
   );
 };
 
@@ -40,22 +38,7 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     paddingBottom: 10,
   },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
 
-    gap: 10,
-    // marginBottom: THEME.spacing.lg,
-    paddingVertical: THEME.spacing.md,
-    borderBottomWidth: 0.6,
-    borderBottomColor: THEME.colors.border,
-  },
-  iconPlaceholder: {
-    width: 24,
-    height: 24,
-    backgroundColor: 'transparent',
-    borderRadius: 12,
-  },
   subtitle: {
     color: THEME.colors.neutral[300],
     marginTop: THEME.spacing.sm,
