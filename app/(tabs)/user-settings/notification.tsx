c43ae683573111c90c71bf7db012f619b7aa433b
Tomiwa Daniel <daniela.stomiwa@yahoo.com>
2024-08-24 13:20:49 +0100

    refactor: Update user settings layout and margins

--- app/(tabs)/user-settings/notification.tsx
+++ app/(tabs)/user-settings/notification.tsx
@@ -1,40 +1,46 @@
-import React, { useEffect, useRef, useState } from 'react';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { StyleSheet, SectionList, SafeAreaView, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  StyleSheet,
+  SectionList,
+  SafeAreaView,
+  TouchableOpacity,
+  ActivityIndicator,
+  Alert,
+} from 'react-native';
 
+import CheckIcon from '~/assets/icons/check.svg';
 import GoBack from '~/components/go-back';
 import RenderSetting from '~/components/notification-settings';
 import { Dialog, DialogRef, Text, View, Button } from '~/components/shared';
+import getNotificationSections from '~/constants/notification';
 import { THEME } from '~/constants/theme';
 import { NotificationSettingsService } from '~/services/notification-settings';
 import useAuthStore from '~/store/auth';
-import getNotificationSections from '~/constants/notification';
-import CheckIcon from '~/assets/icons/check.svg';
 
 const NotificationSettings = () => {
   const authstore = useAuthStore();
   const dialogRef = useRef<DialogRef>(null);
-  const [notificationData, setNotificationData] = useState(getNotificationSections)
+  const [notificationData, setNotificationData] = useState(getNotificationSections);
 
   const { data: fetchedData, isLoading } = useQuery({
     queryKey: ['fetchNotification', authstore.data?.user.id],
-    queryFn: () => NotificationSettingsService.getNotifications(authstore.data?.user.id)
-  })
+    queryFn: () => NotificationSettingsService.getNotifications(authstore.data?.user.id),
+  });
 
   useEffect(() => {
-    if (fetchedData)
-      setNotificationData(getNotificationSections(fetchedData?.data))
-  }, [fetchedData])
+    if (fetchedData) setNotificationData(getNotificationSections(fetchedData?.data));
+  }, [fetchedData]);
 
   const settingsMutation = useMutation({
     mutationFn: () => NotificationSettingsService.setNotifications(notificationData),
     onSuccess: (res) => {
-      dialogRef.current?.open()
+      dialogRef.current?.open();
     },
     onError: (err) => {
-      Alert.alert('Server error', 'An error occured while saving settings')
-    }
-  })
+      Alert.alert('Server error', 'An error occured while saving settings');
+    },
+  });
 
   const toggleSwitch = (sectionIndex: number, itemIndex: number) => {
     setNotificationData((prevState) => {
@@ -45,13 +51,11 @@ const NotificationSettings = () => {
     });
   };
 
-
   return (
     <SafeAreaView style={styles.container}>
-
-      {false ? (
+      {isLoading ? (
         <View style={styles.loadingContainer}>
-          <ActivityIndicator size={'large'} />
+          <ActivityIndicator size="large" />
         </View>
       ) : (
         <>
@@ -95,16 +99,14 @@ const NotificationSettings = () => {
         </>
       )}
 
-
       <Dialog
         ref={dialogRef}
         title="Notification Updated"
         description="Notification preferences updated successfully. Remember, you can always adjust these settings again later"
-        showCloseButton={false}
-      >
+        showCloseButton={false}>
         <View style={styles.dialogButtons}>
           <TouchableOpacity style={styles.cancelButton} onPress={() => dialogRef.current?.close()}>
-            <Text style={styles.cancelButtonText} >Done</Text>
+            <Text style={styles.cancelButtonText}>Done</Text>
           </TouchableOpacity>
         </View>
       </Dialog>
@@ -130,7 +132,7 @@ const styles = StyleSheet.create({
   saveBtn: {
     width: '100%',
     paddingHorizontal: THEME.spacing.md,
-    marginVertical: 10
+    marginVertical: 10,
   },
   section: {
     width: '100%',
@@ -161,14 +163,14 @@ const styles = StyleSheet.create({
   cancelButtonText: {
     color: THEME.colors.white,
     fontSize: THEME.fontSize.lg,
-    fontWeight: 500
+    fontWeight: 500,
   },
   loadingContainer: {
     width: '100%',
     height: '100%',
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
 
 export default NotificationSettings;
